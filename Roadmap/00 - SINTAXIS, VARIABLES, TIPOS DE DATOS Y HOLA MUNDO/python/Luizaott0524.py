5ed513b939794f960e871e9585a883d31f7fdd25
Luizaott0524 <brasilenho@gmail.com>
2024-06-05 21:21:13 +0200

    Create Luizaott0524.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Luizaott0524.py
@@ -0,0 +1,17 @@
+# https://www.python.org
+
+#comentario en una linea 
+""" 
+hola esto es el comentiro en en varias lineas 
+aprendiendo python desde cero
+"""
+´´´
+sigo con la prueba de 
+comentario en varias lineas
+´´´
+
+my_variable = "mi variable"
+my_variable = " nuevo valor de mi variable"
+
+MY_CONSTANT = "MI CONSTANTE "
+MY_CONSTANT = "JAJGAJDAJADKA"
